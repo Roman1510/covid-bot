6f7d0e25e93ae21293611981f3a1dbfd6e4e3e86
Roman1510 <romanvinnick@gmail.com>
2021-05-02 21:29:25 +0200

    remove gitignore files

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require("express")();
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`It's alive on http://localhost:${PORT}`);
+});
+
+app.get('/getStatus',(req,res)=>{
+    res.status(200).send({
+        status: '1',
+        statusCode: 'Success'
+    })
+})
