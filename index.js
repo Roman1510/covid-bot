c30303c061402c21acb06713ae2475d5d0d8f8f0
Roman1510 <romanvinnick@gmail.com>
2021-05-03 22:03:43 +0200

    bot

--- index.js
+++ index.js
@@ -39,41 +39,8 @@ const server = app.listen(process.env.PORT || 5000, () => {
 //----------------------------------------------------------------------------------------------------------------
 const bot = new Telegraf(token)
 
-bot.command('quit', (ctx) => {
-  // Explicit usage
-  ctx.telegram.leaveChat(ctx.message.chat.id)
-
-  // Using context shortcut
-  ctx.leaveChat()
-})
-
-bot.on('text', (ctx) => {
-  // Explicit usage
-  ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
-
-  // Using context shortcut
-  ctx.reply(`Hello ${ctx.state.role}`)
-})
-
-bot.on('callback_query', (ctx) => {
-  // Explicit usage
-  ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
-
-  // Using context shortcut
-  ctx.answerCbQuery()
-})
-
-bot.on('inline_query', (ctx) => {
-  const result = []
-  // Explicit usage
-  ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
-
-  // Using context shortcut
-  ctx.answerInlineQuery(result)
-})
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
 
 bot.launch()
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
