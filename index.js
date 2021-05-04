ec30292e62fe5527926ad4874a3aeab8382d8e5b
Roman1510 <romanvinnick@gmail.com>
2021-05-04 23:20:00 +0200

    the bot idea has changed

--- index.js
+++ index.js
@@ -1,49 +1,30 @@
-const {Telegraf} = require('telegraf');
-const express = require('express');
-const bodyParser = require('body-parser')
-const app = express();
-const path = require('path');
-const token = '1778739993:AAGEOJzxf8uULadGkTBZ2oVmS1VM0B7sRpM'
+const { Telegraf } = require("telegraf");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const covidApi = require("covid19-api");
+
+//const app = express(); yet to be used
+const token = "1778739993:AAGEOJzxf8uULadGkTBZ2oVmS1VM0B7sRpM";
 
 
-app.use(express.static(path.join(__dirname, 'public')));
-var jsonParser = bodyParser.json()
-
-app.get('/json', function(req, res) {
-    console.log("GET the json");
-    console.log(req.body)
-    res
-        .status(200)
-        .json( {"jsonData" : true,
-        "response": `${req.body}`
-      });
-});
-
-app.get('/file', function(req, res) {
-    console.log("GET the file");
-    res
-        .status(200)
-        .sendFile(path.join(__dirname, 'test.txt'));
-});
-
-
-const server = app.listen(process.env.PORT || 5000, () => {
-  const port = server.address().port;
-  console.log(`Express is working on port ${port}`);
-});
-
-
-
-
-//everything about bot starts here:
-//----------------------------------------------------------------------------------------------------------------
-const bot = new Telegraf(token)
-bot.start((ctx) => ctx.reply('/салам\n/help\n или просто напиши hi'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.hears('/салам', (ctx) => ctx.reply('👍 алейкум'+JSON.stringify(ctx.message)))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+const bot = new Telegraf(token);
+bot.on('text', async (ctx) => {
+  try {
+      const userText = ctx.message.text
+      const covidData = await covidApi.getReportsByCountries(userText)
+      console.log(covidData)
+      const countryData = covidData[0][0]
+      const formatData = `
+          Country: ${countryData.country},
+          Cases: ${countryData.cases},
+          Deaths: ${countryData.deaths},
+          Cured: ${countryData.recovered}
+          ${countryData.flag}`
+          
+      ctx.reply(formatData)
+  } catch(e) {
+      ctx.reply('This country doesn\'t exist, please use  /help')
+  }
+})
 bot.launch()
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
