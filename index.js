45ae750242a2139bdf1822b169cce91c3b9e0386
Roman1510 <romanvinnick@gmail.com>
2021-05-05 16:19:34 +0200

    fixed bad behaviour

--- index.js
+++ index.js
@@ -3,28 +3,47 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const covidApi = require("covid19-api");
+const port = "80";
+const app = express();
 
-//const app = express(); yet to be used
+app.get("/", (req, res) => {
+  return res.send("Received a GET HTTP method");
+});
+
+app.post("/", (req, res) => {
+  return res.send("Received a POST HTTP method");
+});
+
+app.put("/", (req, res) => {
+  return res.send("Received a PUT HTTP method");
+});
+
+app.delete("/", (req, res) => {
+  return res.send("Received a DELETE HTTP method");
+});
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`)
+);
+
 const token = "1796374675:AAGaC0athyS7ofoDN8VtZunl_fHuxGL5JwA";
-
-
 const bot = new Telegraf(token);
-bot.on('text', async (ctx) => {
+bot.on("text", async (ctx) => {
   try {
-      const userText = ctx.message.text
-      const covidData = await covidApi.getReportsByCountries(userText)
-      console.log(covidData)
-      const countryData = covidData[0][0]
-      const formatData = `
+    const userText = ctx.message.text;
+    const covidData = await covidApi.getReportsByCountries(userText);
+    console.log(covidData);
+    const countryData = covidData[0][0];
+    const formatData = `
           Country: ${countryData.country},
           Cases: ${countryData.cases},
           Deaths: ${countryData.deaths},
           Cured: ${countryData.recovered}
-          ${countryData.flag}`
-          
-      ctx.reply(formatData)
-  } catch(e) {
-      ctx.reply('This country doesn\'t exist, please use  /help')
+          ${countryData.flag}`;
+
+    ctx.reply(formatData);
+  } catch (e) {
+    ctx.reply("This country doesn't exist, please use  /help");
   }
-})
-bot.launch()
+});
+bot.launch();
