aab95d961cc66beb9ee8ee42bbed21ad9d1a6e6b
Roman1510 <romanvinnick@gmail.com>
2021-05-11 22:32:26 +0200

    refactored to use native api

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
-require('dotenv').config();
-const { Telegraf } = require("telegraf");
+require("dotenv").config();
+const TelegramApi = require("node-telegram-bot-api");
+const token = process.env.API_KEY;
 const covidApi = require("covid19-api");
 const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 //this is just the code to keep the server alive
-
 app.put("/", (req, res) => {
   return res.send("Received a PUT HTTP method");
 });
-
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}}!`));
 
-//here it starts the Telegram-API
-const token = process.env.API_KEY;
-const bot = new Telegraf(token);
-bot.on("text", async (ctx) => {
-  try {
-    const userText = ctx.message.text;
-    const covidData = await covidApi.getReportsByCountries(userText);
-    const countryData = covidData[0][0];
-    const formatData = `
-          Country: ${countryData.country},
-          Cases: ${countryData.cases},
-          Deaths: ${countryData.deaths},
-          Cured: ${countryData.recovered}
-          ${countryData.flag}`;
+const bot = new TelegramApi(token, { polling: true });
+bot.on("message", async (msg) => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
 
-    ctx.reply(formatData);
-  } catch (e) {
-    ctx.reply("This country doesn't exist, please use  /help");
+  if (text === "/start") {
+    await bot.sendSticker(
+      chatId,
+      "https://tlgrm.eu/_/stickers/393/4ef/3934efb0-bf26-4bd9-b826-08dea746eaf3/1.webp"
+    );
+    await bot.sendMessage(
+      chatId,
+      "Welcome to covid19 monitoring bot, just type the country name. For example us, poland, germany etc."
+    );
+  }
+  if (text === "/info") {
+    await bot.sendMessage(
+      chatId,
+      `You are ${msg.chat.first_name} ${msg.chat.last_name || ""}`
+    );
+  }
+  if (text) {
+    try {
+      const covidData = await covidApi.getReportsByCountries(text);
+      const countryData = covidData[0][0];
+      const formatData = `
+            Country: ${countryData.country},
+            Cases: ${countryData.cases},
+            Deaths: ${countryData.deaths},
+            Cured: ${countryData.recovered}
+            ${countryData.flag}`;
+      await bot.sendMessage(chatId, formatData)  
+    } catch(e) {
+      await bot.sendMessage(chatId, "This country doesn't exist, please try again")  
+    }
+    
   }
 });
 
-bot.command("help", (ctx) =>
-  ctx.reply(
-    "Please just message me the name of the country, for example: us, germany, china etc..."
-  )
-);
-bot.launch();
+
