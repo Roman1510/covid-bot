c909dd707cd407fffa3f42037e7ace5a965f990a
Roman1510 <romanvinnick@gmail.com>
2021-05-14 23:40:02 +0200

    fixed the bug

--- index.js
+++ index.js
@@ -46,8 +46,9 @@ bot.on("message", async (msg) => {
     try {
       const covidData = await covidApi.getReportsByCountries(text);
       const countryData = covidData[0][0];
+      const country = name(code(countryData.country));
       const formatData = `
-            Country: ${countryData.country}${flag(countryData.country)}
+            Country: ${country} ${flag(countryData.country)}
             Cases: ${numberWithCommas(countryData.cases)}
             Deaths: ${numberWithCommas(countryData.deaths)}
             Cured: ${numberWithCommas(countryData.recovered)}`;
